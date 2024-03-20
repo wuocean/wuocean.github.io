a2a11765d9560dad534bfaa9013eaebad478d6ab
wuocean <bilywxy@163.com>
2024-03-20 14:43:06 +0800

    Site updated: 2024-03-20 14:43:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/10/24/2023-10-24-总结&新的开始/","2022/12/01/Nacos下载与配置/","2023/09/25/Twikoo评论Vercel 部署/","2022/11/27/hello-word/","2023/06/01/anzhiyu主题标签/","2023/03/28/idea2017-2021激活方案/"];function toRandomPost(){
+var posts=["2023/10/24/2023-10-24-总结&新的开始/","2023/09/25/Twikoo评论Vercel 部署/","2022/12/01/Nacos下载与配置/","2022/11/27/hello-word/","2023/03/28/idea2017-2021激活方案/","2023/06/01/anzhiyu主题标签/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
